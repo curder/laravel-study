2dd2aefe54c79ea18110fc47c4ce6e8835abd773
curder <curder@users.noreply.github.com>
2022-01-07 03:35:14 +0000

    deploy: 809068b1209d6263350e808ec9d61bb9345248d3

---
+++ assets/diff.html.c5af194f.js
@@ -0,0 +1,24 @@
+import{e as n}from"./app.4a314b0f.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const a={},p=n(`<h1 id="diff" tabindex="-1"><a class="header-anchor" href="#diff" aria-hidden="true">#</a> diff</h1><p>\u5C06\u96C6\u5408\u4E0E\u5176\u5B83\u96C6\u5408\u6216\u7EAF PHP \u6570\u7EC4\u8FDB\u884C\u503C\u7684\u6BD4\u8F83\uFF0C\u7136\u540E\u8FD4\u56DE\u539F\u96C6\u5408\u4E2D\u5B58\u5728\u800C\u7ED9\u5B9A\u96C6\u5408\u4E2D\u4E0D\u5B58\u5728\u7684\u503C\u3002</p><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token variable">$collection</span> <span class="token operator">=</span> <span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token number">1</span><span class="token punctuation">,</span> <span class="token number">2</span><span class="token punctuation">,</span> <span class="token number">3</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token variable">$collection</span><span class="token operator">-&gt;</span><span class="token function">diff</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token number">2</span><span class="token punctuation">,</span> <span class="token number">4</span><span class="token punctuation">,</span> <span class="token number">6</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token comment">// output</span>
+<span class="token doc-comment comment">/**
+=&gt; Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1087
+     all: [
+       0 =&gt; 1,
+       2 =&gt; 3,
+     ],
+   <span class="token punctuation">}</span>
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br></div></div><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token variable">$collection</span> <span class="token operator">=</span> <span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token string single-quoted-string">&#39;apples&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;bananas&#39;</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token variable">$collection</span><span class="token operator">-&gt;</span><span class="token function">diff</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token string single-quoted-string">&#39;pears&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;bananas&#39;</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token comment">// output</span>
+<span class="token doc-comment comment">/**
+=&gt; Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1083
+     all: [
+       &quot;apples&quot;,
+     ],
+   <span class="token punctuation">}</span>
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br></div></div>`,4);function e(t,c){return p}var u=s(a,[["render",e]]);export{u as default};
