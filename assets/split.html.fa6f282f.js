6ce09def91351ec7ce6cbcb1d1291f692c3e1660
curder <curder@users.noreply.github.com>
2022-01-18 03:20:58 +0000

    deploy: 5711b7aac5bb939f7a2352f7bd0a6e5284a7cf19

---
+++ assets/split.html.fa6f282f.js
@@ -0,0 +1,26 @@
+import{e as n}from"./app.0c9bc181.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const a={},p=n(`<h1 id="split" tabindex="-1"><a class="header-anchor" href="#split" aria-hidden="true">#</a> split</h1><p>\u5C06\u96C6\u5408\u6309\u7ED9\u5B9A\u7684\u503C\u62C6\u5206\u3002</p><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token number">1</span><span class="token punctuation">,</span> <span class="token number">2</span><span class="token punctuation">,</span> <span class="token number">3</span><span class="token punctuation">,</span> <span class="token number">4</span><span class="token punctuation">,</span> <span class="token number">5</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token operator">-&gt;</span><span class="token function">split</span><span class="token punctuation">(</span><span class="token number">3</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token doc-comment comment">/**
+=&gt; Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1077
+     all: [
+       Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1076
+         all: [
+           1,
+           2,
+         ],
+       <span class="token punctuation">}</span>,
+       Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1075
+         all: [
+           3,
+           4,
+         ],
+       <span class="token punctuation">}</span>,
+       Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1074
+         all: [
+           5,
+         ],
+       <span class="token punctuation">}</span>,
+     ],
+   <span class="token punctuation">}</span>
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br><span class="line-number">24</span><br><span class="line-number">25</span><br></div></div>`,3);function e(l,t){return p}var r=s(a,[["render",e]]);export{r as default};
