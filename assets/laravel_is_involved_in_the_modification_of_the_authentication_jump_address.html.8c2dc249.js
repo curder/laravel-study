da2c1cf222c88c204ad00b8950a1e07e99d2773b
curder <curder@users.noreply.github.com>
2022-01-20 03:02:58 +0000

    deploy: 7f99e05adac1e4ebb4ae738174d2b42d159c4d6a

---
+++ assets/laravel_is_involved_in_the_modification_of_the_authentication_jump_address.html.8c2dc249.js
@@ -0,0 +1,17 @@
+import{e}from"./app.cbda6c6d.js";import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";const a={},s=e(`<h2 id="laravel\u4E2D\u6D89\u53CA\u8BA4\u8BC1\u8DF3\u8F6C\u5730\u5740\u7684\u4FEE\u6539\u7684\u5730\u65B9" tabindex="-1"><a class="header-anchor" href="#laravel\u4E2D\u6D89\u53CA\u8BA4\u8BC1\u8DF3\u8F6C\u5730\u5740\u7684\u4FEE\u6539\u7684\u5730\u65B9" aria-hidden="true">#</a> Laravel\u4E2D\u6D89\u53CA\u8BA4\u8BC1\u8DF3\u8F6C\u5730\u5740\u7684\u4FEE\u6539\u7684\u5730\u65B9</h2><p>\u767B\u5F55\u540E\u8DF3\u8F6C\uFF1A</p><p>\u5728<code>app\\Http\\Auth\\LoginController.php</code>\u4E2D\u4FEE\u6539</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>protected $redirectTo = &#39;/home&#39;;
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br></div></div><p>\u5DF2\u767B\u5F55\uFF0C\u8BBF\u95EE\u767B\u5F55\u63A5\u53E3\u65F6\u8DF3\u8F6C\uFF1A</p><p>\u4FEE\u6539<code>app\\Http\\Middleware\\RedirectIfAuthenticated.php</code>\u4E2D\u7684<code>handle()</code>\u65B9\u6CD5\uFF1A</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code> public function handle($request, Closure $next, $guard = null)
+ {
+   if (Auth::guard($guard)-&gt;check()) {
+       return redirect(&#39;/home&#39;);
+   }
+   
+   return $next($request);
+ }
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br></div></div><p>\u672A\u767B\u5F55\u8BBF\u95EE\u9700\u8981\u767B\u5F55\u9875\u9762\u65F6\u7684\u8DF3\u8F6C</p><p>\u4FEE\u6539<code>app\\Http\\Exceptions\\Handler.php</code>\u4E2D\u7684<code>unauthenticated()</code>\u65B9\u6CD5\uFF1A</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>protected function unauthenticated($request, AuthenticationException $exception)
+{
+    if ($request-&gt;expectsJson()) {
+        return response()-&gt;json([&#39;error&#39; =&gt; &#39;Unauthenticated.&#39;], 401);
+    }
+    return redirect()-&gt;guest(route(&#39;login&#39;));
+}
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br></div></div>`,10);function r(t,p){return s}var i=n(a,[["render",r]]);export{i as default};
---
+++ assets/takeUntil.html.dee2ef2a.js
@@ -0,0 +1,23 @@
+import{e as n}from"./app.cbda6c6d.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const a={},p=n(`<h1 id="takeuntil" tabindex="-1"><a class="header-anchor" href="#takeuntil" aria-hidden="true">#</a> takeUntil</h1><p>\u5C06\u8FD4\u56DE\u96C6\u5408\u4E2D\u7684\u9879\u76EE\uFF0C\u76F4\u5230\u7ED9\u5B9A\u7684\u56DE\u8C03\u8FD4\u56DE <code>true</code>\u3002</p><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token number">1</span><span class="token punctuation">,</span> <span class="token number">2</span><span class="token punctuation">,</span> <span class="token number">3</span><span class="token punctuation">,</span> <span class="token number">4</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token operator">-&gt;</span><span class="token function">takeUntil</span><span class="token punctuation">(</span><span class="token keyword">function</span> <span class="token punctuation">(</span><span class="token variable">$item</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+  <span class="token keyword">return</span> <span class="token variable">$item</span> <span class="token operator">&gt;=</span> <span class="token number">3</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token doc-comment comment">/**
+=&gt; Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1080
+     all: [
+       1,
+       2,
+     ],
+   <span class="token punctuation">}</span>
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br></div></div><p>\u4E5F\u53EF\u4EE5\u5C06\u7B80\u5355\u7684\u503C\u4F20\u9012\u7ED9 <code>takeUntil</code> \u65B9\u6CD5\u4EE5\u83B7\u53D6\u9879\u76EE\uFF0C\u76F4\u5230\u627E\u5230\u7ED9\u5B9A\u7684\u503C\u3002</p><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token number">1</span><span class="token punctuation">,</span> <span class="token number">2</span><span class="token punctuation">,</span> <span class="token number">3</span><span class="token punctuation">,</span> <span class="token number">4</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token operator">-&gt;</span><span class="token function">takeUntil</span><span class="token punctuation">(</span><span class="token number">3</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token doc-comment comment">/**
+=&gt; Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1075
+     all: [
+       1,
+       2,
+     ],
+   <span class="token punctuation">}</span>
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br></div></div><blockquote><p><strong>\u6CE8\u610F\uFF1A</strong> \u5982\u679C\u627E\u4E0D\u5230\u7ED9\u5B9A\u7684\u503C\u6216\u56DE\u8C03\u4ECE\u672A\u8FD4\u56DE <code>true</code> \uFF0C\u5219 <code>takeUntil</code> \u65B9\u6CD5\u5C06\u8FD4\u56DE\u96C6\u5408\u4E2D\u7684\u6240\u6709\u9879\u76EE\u3002</p></blockquote>`,6);function e(t,c){return p}var u=s(a,[["render",e]]);export{u as default};
