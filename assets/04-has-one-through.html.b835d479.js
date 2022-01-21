64effd7f51954d8a00a1fd7a7e3e652448d88e0c
curder <curder@users.noreply.github.com>
2022-01-21 13:54:34 +0000

    deploy: 73a420b959478efbf58107add644368b82a8e37f

---
+++ assets/04-has-one-through.html.b835d479.js
@@ -0,0 +1,34 @@
+import{e as n}from"./app.c8572b8c.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const e={},a=n(`<h1 id="\u8FDC\u5C42\u4E00\u5BF9\u4E00" tabindex="-1"><a class="header-anchor" href="#\u8FDC\u5C42\u4E00\u5BF9\u4E00" aria-hidden="true">#</a> \u8FDC\u5C42\u4E00\u5BF9\u4E00</h1><p>\u9884\u6848\u5C42\u4E00\u5BF9\u4E00\u5173\u8054\u901A\u8FC7\u5355\u4E00\u4E2D\u95F4\u5173\u7CFB\u94FE\u63A5\u6A21\u578B\u3002</p><p>\u4F8B\u5982\uFF0C\u5982\u679C\u6BCF\u4E2A\u4F9B\u5E94\u5546\u90FD\u6709\u4E00\u4E2A\u7528\u6237\uFF0C\u540C\u65F6\u6BCF\u4E2A\u7528\u6237\u90FD\u4E0E\u4E00\u4E2A\u7528\u6237\u5386\u53F2\u8BB0\u5F55\u76F8\u5173\u8054\uFF0C\u8FD9\u6837\uFF0C\u4F9B\u5E94\u5546\u6A21\u578B\u5C31\u53EF\u4EE5\u901A\u8FC7\u7528\u6237\u6765\u8BBF\u95EE\u7528\u6237\u7684\u5386\u53F2\u3002\u4E0B\u9762\u6211\u4EEC\u6765\u770B\u770B\u5B9A\u4E49\u8FD9\u4E2A\u5173\u8054\u5173\u7CFB\u6240\u9700\u7684\u6570\u636E\u8868\u7ED3\u6784\uFF1A</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>users
+    id - integer
+    supplier_id - integer
+
+suppliers
+    id - integer
+
+history
+    id - integer
+    user_id - integer
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br></div></div><p><code>history</code> \u6570\u636E\u8868\u4E0D\u5305\u542B <code>supplier_id</code> \u5217\uFF0C<code>hasOneThrough</code> \u5173\u8054\u4ECD\u7136\u53EF\u4EE5\u4E3A\u4F9B\u5E94\u5546\u63D0\u4F9B\u5BF9\u7528\u6237\u5386\u53F2\u7684\u8BBF\u95EE\u3002</p><p>\u73B0\u5728\uFF0C\u5DF2\u7ECF\u77E5\u9053\u5173\u8054\u5173\u7CFB\u5BF9\u5E94\u7684\u6570\u636E\u8868\u7ED3\u6784\uFF0C\u63A5\u4E0B\u6765\u5728 <code>Supplier</code> \u6A21\u578B\u4E0A\u5B9A\u4E49\u8FD9\u4E2A\u5173\u8054</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>&lt;?php
+
+namespace App;
+
+use Illuminate\\Database\\Eloquent\\Model;
+
+class Supplier extends Model
+{
+    /**
+     * Get the user&#39;s history.
+     */
+    public function userHistory()
+    {
+        return $this-&gt;hasOneThrough(
+            &#39;App\\History&#39;,
+            &#39;App\\User&#39;,
+            &#39;supplier_id&#39;, // Foreign key on users table...
+            &#39;user_id&#39;, // Foreign key on history table...
+            &#39;id&#39;, // Local key on suppliers table...
+            &#39;id&#39; // Local key on users table...
+            );
+    }
+}
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br></div></div>`,7);function r(p,l){return a}var c=s(e,[["render",r]]);export{c as default};
---
+++ assets/sort.html.f86d5d4f.js
@@ -0,0 +1,63 @@
+import{r as t,o as e,c as o,b as s,a as p,F as c,e as l,d as n}from"./app.c8572b8c.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";const u={},i=l(`<h1 id="sort" tabindex="-1"><a class="header-anchor" href="#sort" aria-hidden="true">#</a> sort</h1><p>\u5BF9\u96C6\u5408\u8FDB\u884C\u6392\u5E8F\u3002</p><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token number">5</span><span class="token punctuation">,</span> <span class="token number">7</span><span class="token punctuation">,</span> <span class="token number">3</span><span class="token punctuation">,</span> <span class="token number">9</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token operator">-&gt;</span><span class="token function">sort</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token comment">// output</span>
+<span class="token doc-comment comment">/**
+=&gt; Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1209
+     all: [
+       2 =&gt; 3,
+       0 =&gt; 5,
+       1 =&gt; 7,
+       3 =&gt; 9,
+     ],
+   <span class="token punctuation">}</span>
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br></div></div><p>\u4F7F\u7528 sort \u5BF9\u96C6\u5408\u6392\u5E8F\u540E\uFF0C\u65B0\u8FD4\u56DE\u7684\u96C6\u5408\u4F1A\u4FDD\u7559\u539F\u6765\u96C6\u5408\u7684 key\u3002</p><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token string single-quoted-string">&#39;A55&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;B54&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;B22&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;A11&#39;</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token operator">-&gt;</span><span class="token function">sort</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token comment">// output</span>
+<span class="token doc-comment comment">/**
+=&gt; Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1209
+     all: [
+       3 =&gt; &quot;A11&quot;,
+       0 =&gt; &quot;A55&quot;,
+       2 =&gt; &quot;B22&quot;,
+       1 =&gt; &quot;B54&quot;,
+     ],
+   <span class="token punctuation">}</span>
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br></div></div><p>\u8C03\u7528 <code>sort</code> \u65B9\u6CD5\u4E0D\u4F20\u9012\u4EFB\u4F55\u53C2\u6570\u9ED8\u8BA4\u6392\u5E8F\u4F7F\u7528\u503C\u7684asci\u7F16\u7801\u8FDB\u884C\u6392\u5E8F\u3002</p><p>\u81EA\u5B9A\u4E49\u56DE\u8C03\u51FD\u6570\u6392\u5E8F</p><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token string single-quoted-string">&#39;A-55&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;B54&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;B-22&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;A11&#39;</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token operator">-&gt;</span><span class="token function">sort</span><span class="token punctuation">(</span><span class="token keyword">fn</span> <span class="token punctuation">(</span><span class="token variable">$a</span><span class="token punctuation">,</span> <span class="token variable">$b</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span>  <span class="token function">str_replace</span><span class="token punctuation">(</span><span class="token string single-quoted-string">&#39;-&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;&#39;</span><span class="token punctuation">,</span> <span class="token variable">$a</span><span class="token punctuation">)</span> <span class="token operator">&lt;</span> <span class="token variable">$b</span> <span class="token operator">?</span> <span class="token operator">-</span><span class="token number">1</span> <span class="token punctuation">:</span> <span class="token number">1</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token comment">// output</span>
+<span class="token doc-comment comment">/**
+=&gt; Illuminate\\Support\\Collection <span class="token punctuation">{</span>#1228
+     all: [
+       3 =&gt; &quot;A11&quot;,
+       0 =&gt; &quot;A-55&quot;,
+       2 =&gt; &quot;B-22&quot;,
+       1 =&gt; &quot;B54&quot;,
+     ],
+   <span class="token punctuation">}</span>
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br></div></div><h2 id="\u4E0D\u6539\u53D8\u539F\u96C6\u5408" tabindex="-1"><a class="header-anchor" href="#\u4E0D\u6539\u53D8\u539F\u96C6\u5408" aria-hidden="true">#</a> \u4E0D\u6539\u53D8\u539F\u96C6\u5408</h2><div class="language-php ext-php line-numbers-mode"><pre class="language-php"><code><span class="token variable">$collection</span> <span class="token operator">=</span> <span class="token function">collect</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token string single-quoted-string">&#39;A-55&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;B54&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;B-22&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;A11&#39;</span><span class="token punctuation">]</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token variable">$newCollection</span> <span class="token operator">=</span> <span class="token variable">$collection</span><span class="token operator">-&gt;</span><span class="token function">sort</span><span class="token punctuation">(</span><span class="token keyword">fn</span> <span class="token punctuation">(</span><span class="token variable">$a</span><span class="token punctuation">,</span> <span class="token variable">$b</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span>  <span class="token function">str_replace</span><span class="token punctuation">(</span><span class="token string single-quoted-string">&#39;-&#39;</span><span class="token punctuation">,</span> <span class="token string single-quoted-string">&#39;&#39;</span><span class="token punctuation">,</span> <span class="token variable">$a</span><span class="token punctuation">)</span> <span class="token operator">&lt;</span> <span class="token variable">$b</span> <span class="token operator">?</span> <span class="token operator">-</span><span class="token number">1</span> <span class="token punctuation">:</span> <span class="token number">1</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token variable">$newCollection</span><span class="token operator">-&gt;</span><span class="token function">dump</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token variable">$collection</span><span class="token operator">-&gt;</span><span class="token function">dd</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+<span class="token comment">// output</span>
+<span class="token doc-comment comment">/**
+array:4 [
+  3 =&gt; &quot;A11&quot;
+  0 =&gt; &quot;A-55&quot;
+  2 =&gt; &quot;B-22&quot;
+  1 =&gt; &quot;B54&quot;
+]
+
+array:4 [
+  0 =&gt; &quot;A-55&quot;
+  1 =&gt; &quot;B54&quot;
+  2 =&gt; &quot;B-22&quot;
+  3 =&gt; &quot;A11&quot;
+] 
+ */</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br></div></div>`,10),b=n("\u5982\u679C\u6709\u66F4\u9AD8\u7EA7\u7684\u6392\u5E8F\u9700\u6C42\uFF0C\u4F60\u53EF\u4EE5\u4F20\u5165\u56DE\u8C03\u6765\u7528\u4F60\u81EA\u5DF1\u7684\u7B97\u6CD5\u8FDB\u884C\u6392\u5E8F\u3002\u8BF7\u53C2\u9605 PHP \u6587\u6863\u7684 "),k={href:"http://php.net/manual/en/function.usort.php#refsect1-function.usort-parameters",target:"_blank",rel:"noopener noreferrer"},m=n("usort"),g=n("\uFF0C\u8FD9\u662F\u96C6\u5408\u7684 "),d=s("code",null,"sort",-1),q=n(" \u65B9\u6CD5\u5728\u5E95\u5C42\u6240\u8C03\u7528\u7684\u3002"),h=n("\u5982\u679C\u8981\u5BF9\u5D4C\u5957\u6570\u7EC4\u6216\u5BF9\u8C61\u7684\u96C6\u5408\u8FDB\u884C\u6392\u5E8F\uFF0C\u53C2\u8003 "),_={href:"/collections/sortBy.md",target:"_blank",rel:"noopener noreferrer"},f=n("sortBy"),v=n(" \u548C "),B={href:"/collections/sortByDesc.md",target:"_blank",rel:"noopener noreferrer"},A=n("sortByDesc"),x=n(" \u65B9\u6CD5\u3002");function $(y,C){const a=t("ExternalLinkIcon");return e(),o(c,null,[i,s("p",null,[b,s("a",k,[m,p(a)]),g,d,q]),s("p",null,[h,s("a",_,[f,p(a)]),v,s("a",B,[A,p(a)]),x])],64)}var N=r(u,[["render",$]]);export{N as default};
