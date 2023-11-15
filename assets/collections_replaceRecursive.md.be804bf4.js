fdff0781e616a5b3c638815d077c82334c42a181
curder <curder@users.noreply.github.com>
2023-11-15 07:36:25 +0000

    deploy: 9e8f2e2c7932f494d227f0315a324f3c6250d978

---
+++ assets/collections_replaceRecursive.md.be804bf4.js
@@ -0,0 +1,27 @@
+import{_ as s,o as a,c as n,Q as l}from"./chunks/framework.16715cb7.js";const h=JSON.parse('{"title":"replaceRecursive","description":"","frontmatter":{},"headers":[],"relativePath":"collections/replaceRecursive.md","filePath":"collections/replaceRecursive.md","lastUpdated":1696669217000}'),p={name:"collections/replaceRecursive.md"},e=l(`<h1 id="replacerecursive" tabindex="-1">replaceRecursive <a class="header-anchor" href="#replacerecursive" aria-label="Permalink to &quot;replaceRecursive&quot;">​</a></h1><p>此方法的工作方式类似于<a href="./replace.html"><code>replace</code></a>，但它将递归到数组中并且对内部的值应用相同的替换。</p><div class="language-php vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">php</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#B392F0;">collect</span><span style="color:#E1E4E8;">([</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#9ECBFF;">&#39;Taylor&#39;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#9ECBFF;">&#39;Abigail&#39;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">    [</span></span>
+<span class="line"><span style="color:#E1E4E8;">        </span><span style="color:#9ECBFF;">&#39;James&#39;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">        </span><span style="color:#9ECBFF;">&#39;Victoria&#39;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">        </span><span style="color:#9ECBFF;">&#39;Finn&#39;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    ]</span></span>
+<span class="line"><span style="color:#E1E4E8;">])</span><span style="color:#F97583;">-&gt;</span><span style="color:#B392F0;">replaceRecursive</span><span style="color:#E1E4E8;">([</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#9ECBFF;">&#39;Charlie&#39;</span><span style="color:#E1E4E8;">,</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">2</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=&gt;</span><span style="color:#E1E4E8;"> [</span><span style="color:#79B8FF;">1</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=&gt;</span><span style="color:#E1E4E8;"> </span><span style="color:#9ECBFF;">&#39;King&#39;</span><span style="color:#E1E4E8;">]</span></span>
+<span class="line"><span style="color:#E1E4E8;">]);</span></span>
+<span class="line"><span style="color:#E1E4E8;"> </span></span>
+<span class="line"><span style="color:#6A737D;">// [&#39;Charlie&#39;, &#39;Abigail&#39;, [&#39;James&#39;, &#39;King&#39;, &#39;Finn&#39;]]</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#6F42C1;">collect</span><span style="color:#24292E;">([</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#032F62;">&#39;Taylor&#39;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#032F62;">&#39;Abigail&#39;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">    [</span></span>
+<span class="line"><span style="color:#24292E;">        </span><span style="color:#032F62;">&#39;James&#39;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">        </span><span style="color:#032F62;">&#39;Victoria&#39;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">        </span><span style="color:#032F62;">&#39;Finn&#39;</span></span>
+<span class="line"><span style="color:#24292E;">    ]</span></span>
+<span class="line"><span style="color:#24292E;">])</span><span style="color:#D73A49;">-&gt;</span><span style="color:#6F42C1;">replaceRecursive</span><span style="color:#24292E;">([</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#032F62;">&#39;Charlie&#39;</span><span style="color:#24292E;">,</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#005CC5;">2</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=&gt;</span><span style="color:#24292E;"> [</span><span style="color:#005CC5;">1</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">=&gt;</span><span style="color:#24292E;"> </span><span style="color:#032F62;">&#39;King&#39;</span><span style="color:#24292E;">]</span></span>
+<span class="line"><span style="color:#24292E;">]);</span></span>
+<span class="line"><span style="color:#24292E;"> </span></span>
+<span class="line"><span style="color:#6A737D;">// [&#39;Charlie&#39;, &#39;Abigail&#39;, [&#39;James&#39;, &#39;King&#39;, &#39;Finn&#39;]]</span></span></code></pre></div><h2 id="相关方法" tabindex="-1">相关方法 <a class="header-anchor" href="#相关方法" aria-label="Permalink to &quot;相关方法&quot;">​</a></h2><ul><li><a href="./replace.html">replace</a></li></ul>`,5),o=[e];function c(r,t,E,i,y,F){return a(),n("div",null,o)}const u=s(p,[["render",c]]);export{h as __pageData,u as default};
