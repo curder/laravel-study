532232f6d991e6c0db745946c4890a6d9158ee5a
curder <q.curder@gmail.com>
2024-06-20 13:53:36 +0800

    WIP

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,44 +1,43 @@
-import {defineConfig} from 'vitepress'
-import nav from './nav'
+import {defineConfig} from "vitepress"
+import nav from "./nav"
 import sidebar from "./sidebar";
 
 export default defineConfig({
     base: "/laravel-study/",
-    lang: 'zh-CN',
-    title: 'Laravel 学习记录',
-    description: 'Laravel学习点滴',
+    lang: "zh-CN",
+    title: "Laravel 学习记录",
+    description: "Laravel 学习点滴",
     lastUpdated: true,
     head: [
-        ['link', {rel: 'icon', href: '/laravel-study/images/favicon.ico'}],
+        ["link", {rel: "icon", href: "/laravel-study/images/favicon.ico"}],
     ],
-
     themeConfig: {
         search: {
-            provider: 'local',
+            provider: "local",
         },
         logo: {
-            src: '/images/logomark.min.svg',
+            src: "/images/logomark.min.svg",
             alt: "Laravel Logo",
         },
         siteTitle: "Laravel 学习记录",
         outlineBadges: false,
         outline: {
             label: "章节导航",
-            level: 'deep',
+            level: "deep",
         },
         lastUpdated: {
             text: "最后更新",
         },
         docFooter: {
-            prev: '上一页',
-            next: '下一页'
+            prev: "上一页",
+            next: "下一页"
         },
         editLink: {
             pattern: "https://github.com/curder/laravel-study/edit/master/docs/:path",
-            text: '编辑它'
+            text: "编辑它"
         },
         socialLinks: [
-            {icon: 'github', link: 'https://github.com/curder/laravel-study'}
+            {icon: "github", link: "https://github.com/curder/laravel-study"}
         ],
         // @ts-ignore
         nav,
