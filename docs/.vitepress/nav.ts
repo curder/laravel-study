6c64a25c8605907f52455ef28628bd5eec3f22cd
curder <q.curder@gmail.com>
2024-09-12 10:34:13 +0800

    Change navigation links

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -1,21 +1,26 @@
 export default [
     {text: "集合", link: "/collections/"},
     {text: "模型", link: "/model/"},
-    {
-        text: "更多",
-        items: [
-            {text: "代码片段", link: "/partials/migrations"},
-            {text: "小技巧", link: "/tips/db-models-and-eloquent"},
-            {text: "常用扩展", link: "/packages/"},
-            {text: "其他", link: "/others/whats-new-in-laravel-11"},
-        ]
-    },
+    {text: "代码片段", link: "/partials/migrations"},
+    {text: "小技巧", link: "/tips/db-models-and-eloquent"},
+    {text: "常用扩展", link: "/packages/"},
+    {text: "其他", link: "/others/whats-new-in-laravel-11"},
     {
         text: "",
         items: [
             {text: "Bolg", link: "https://curder.github.io/blog/", target: "_blank", rel: "blog"},
-            {text: "What is new in PHP", link: "https://curder.github.io/what-is-new-in-php/", target: "_blank", rel: "what is new in php"},
-            {text: "Setup mac for developer", link: "https://curder.github.io/setup-mac-for-developer/", target: "_blank", rel: "setup mac for developer"},
+            {
+                text: "What is new in PHP",
+                link: "https://curder.github.io/what-is-new-in-php/",
+                target: "_blank",
+                rel: "what is new in php"
+            },
+            {
+                text: "Setup mac for developer",
+                link: "https://curder.github.io/setup-mac-for-developer/",
+                target: "_blank",
+                rel: "setup mac for developer"
+            },
         ]
     },
 ];
