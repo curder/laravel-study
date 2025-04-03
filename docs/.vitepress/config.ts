4323417cd54c33c6f0f75b0928e433cc9fe43297
curder <q.curder@gmail.com>
2025-04-03 15:51:04 +0800

    feat(docs/.vitepress/config.ts): Add themeConfig options
    The commit adds various themeConfig options like search provider, logo, siteTitle, outline, lastUpdated, docFooter, editLink, and socialLinks. This is to enhance the user experience and functionality of the documentation site.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,46 +1,43 @@
-import {defineConfig} from "vitepress"
-import nav from "./nav"
+import { defineConfig } from "vitepress";
+import nav from "./nav";
 import sidebar from "./sidebar";
 
 export default defineConfig({
-    base: "/laravel-study/",
-    lang: "zh-CN",
-    title: "Laravel 学习记录",
-    description: "Laravel 学习点滴",
-    lastUpdated: true,
-    head: [
-        ["link", {rel: "icon", href: "/laravel-study/images/favicon.ico"}],
+  base: "/laravel-study/",
+  lang: "zh-CN",
+  title: "Laravel 学习记录",
+  description: "Laravel 学习点滴",
+  lastUpdated: true,
+  head: [["link", { rel: "icon", href: "/laravel-study/images/favicon.ico" }]],
+  themeConfig: {
+    search: {
+      provider: "local",
+    },
+    logo: {
+      src: "/images/logomark.min.svg",
+      alt: "Laravel Logo",
+    },
+    siteTitle: "Laravel 学习记录",
+    outline: {
+      label: "章节导航",
+      level: "deep",
+    },
+    lastUpdated: {
+      text: "最后更新",
+    },
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+    editLink: {
+      pattern: "https://github.com/curder/laravel-study/edit/master/docs/:path",
+      text: "编辑它",
+    },
+    socialLinks: [
+      { icon: "github", link: "https://github.com/curder/laravel-study" },
     ],
-    themeConfig: {
-        search: {
-            provider: "local",
-        },
-        logo: {
-            src: "/images/logomark.min.svg",
-            alt: "Laravel Logo",
-        },
-        siteTitle: "Laravel 学习记录",
-        outlineBadges: false,
-        outline: {
-            label: "章节导航",
-            level: "deep",
-        },
-        lastUpdated: {
-            text: "最后更新",
-        },
-        docFooter: {
-            prev: "上一页",
-            next: "下一页"
-        },
-        editLink: {
-            pattern: "https://github.com/curder/laravel-study/edit/master/docs/:path",
-            text: "编辑它"
-        },
-        socialLinks: [
-            {icon: "github", link: "https://github.com/curder/laravel-study"}
-        ],
-        // @ts-ignore
-        nav,
-        sidebar,
-    }
+    // @ts-ignore
+    nav,
+    sidebar,
+  },
 });
